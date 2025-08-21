b362e54594d1551856436eb401a394be33dbf3a6
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-08-21 11:55:41 -0300

    chore: update example projects elixir deps, use fixed otp version and stable version for CLI release

--- priv/dev/client/mix.exs
+++ priv/dev/client/mix.exs
@@ -5,7 +5,7 @@ defmodule Incomer.MixProject do
     [
       app: :incomer,
       version: "0.1.0",
-      elixir: "~> 1.19-rc",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
