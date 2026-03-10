955e537f71d82b3bac46d33a00181b1fa06dcdae
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2026-03-10 22:16:53 +0200

    Fix misleading session store warning when disabled

--- lib/anubis/application.ex
+++ lib/anubis/application.ex
@@ -28,21 +28,36 @@ defmodule Anubis.Application do
   end
 
   defp maybe_start_session_store do
-    if adapter = Anubis.get_session_store_adapter() do
-      config = Application.get_env(:anubis_mcp, :session_store)
+    config = Application.get_env(:anubis_mcp, :session_store, [])
+    session_store_children(config)
+  end
 
-      Anubis.Logging.log(:info, "Starting session store",
-        enabled: true,
-        adapter: adapter,
-        ttl: Keyword.get(config, :ttl),
-        namespace: Keyword.get(config, :namespace)
-      )
+  @doc false
+  def session_store_children(config) when is_list(config) do
+    enabled? = Keyword.get(config, :enabled, false)
+    adapter = Keyword.get(config, :adapter)
 
-      [{adapter, config}]
-    else
-      Anubis.Logging.log(:warning, "Session store enabled but adapter not available", adapter: adapter)
+    cond do
+      not enabled? ->
+        []
 
-      []
+      is_nil(adapter) ->
+        Anubis.Logging.log(:warning, "Session store enabled but adapter not configured", [])
+        []
+
+      Code.ensure_loaded?(adapter) ->
+        Anubis.Logging.log(:info, "Starting session store",
+          enabled: true,
+          adapter: adapter,
+          ttl: Keyword.get(config, :ttl),
+          namespace: Keyword.get(config, :namespace)
+        )
+
+        [{adapter, config}]
+
+      true ->
+        Anubis.Logging.log(:warning, "Session store enabled but adapter not available", adapter: adapter)
+        []
     end
   end
 end
